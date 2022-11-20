37cfc7452679800e0aa146009aaa2de114787da5
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-11-20 02:07:03 +0000

    Installed Bootstrap and Axios

---
+++ rm-portfolio/src/App.js
@@ -0,0 +1,9 @@
+// Imports go Here
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import Splash from "./Splash";
+//import "./App.css";
+import Navbar from "./Navbar";
+import Specials from "./Specials";
+import Footer from "./Footer";
